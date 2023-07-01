a2e005d9891329a6bd44fd7345a79f9ca2416d4f
Xuanchen Qian <xuancq@ad.unc.edu>
2023-07-01 11:03:34 -0400

    Model added; About continued.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 1)}
+      ></motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm an undergraduate student currently seeking a BS degree in Math and
+        Computer Science at UNC-Chapel Hill. I'm a skilled developer,
+        math-lover, and quant trader with experience in JavaScript, Java,
+        Python, and expertise in frameworks like React, Node.js, and Electron.
+        I'm a quick learner and enthusiastic in creating efficient,
+        user-friendly, and scalable solutions/algorithms that solve real-world
+        problems. Let's work together and make our ideas alive!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
